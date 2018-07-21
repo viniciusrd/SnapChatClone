ffdb06928bb1592c7399c0c05a1090d85bdd8e71
Vinícius Rodrigues <vinird1993@gmail.com>
2018-07-21 19:51:14 -0300

    initialize version android

---
+++ VersaoAndroid/app/src/main/java/br/com/vinicius/android/snapchatclone/view/SnapsActivity.java
@@ -0,0 +1,15 @@
+package br.com.vinicius.android.snapchatclone.view;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+        import br.com.vinicius.android.snapchatclone.R;
+
+public class SnapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snaps);
+    }
+}
